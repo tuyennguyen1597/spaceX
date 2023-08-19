b817abe7dafc942be4533d3d2d6df5ccfe45e18c
tuyennguyen1597 <61477665+tuyennguyen1597@users.noreply.github.com>
2023-08-19 18:02:05 +1000

    implemented filter option (WIP)

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './App.css';
+import { Filter } from './component/filter';
+
+function App() {
+  return (
+    <Fragment>
+      <Filter/>
+    </Fragment>
+  );
+}
+
+export default App;
